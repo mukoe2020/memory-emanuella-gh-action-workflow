70d9f14398dc10df4e0ee8ab457c6c9e9abd8e2e
memory <memoemukoe@gmail.com>
2026-08-04 23:33:13 +0200

    Add security vulnerability

--- src/components/OctoLink.tsx
+++ src/components/OctoLink.tsx
@@ -5,13 +5,11 @@ interface OctoLinkProps {
   title: string;
 }
 
-function sanitizeUrl(url: string) {
-  // UNCOMMENT THE FOLLOWING LINES TO INTRODUCE A SECURITY VULNERABILITY FOR STEP 04: SECURITY
-   const u = decodeURI(url).trim().toLowerCase();
-   if (u.startsWith("javascript:")) {
-    return "about:blank";
-   }
-  return url;
+function sanitizeUrl(url: string): string {
+    let u = decodeURI(url).trim().toLowerCase();
+    if (u.startsWith("javascript:") || u.startsWith("data:") || u.startsWith("vbscript:"))
+        return "about:blank";
+    return url;
 }
 
 const OctoLink: FunctionComponent<OctoLinkProps> = ({
